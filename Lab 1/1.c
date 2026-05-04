a2b2e919e581ae324dd23f578d2d8e4ab2aaeaab
Dhruv Limbasiya <24031701027@darshan.ac.in>
2026-05-04 22:02:25 +0530

    Lab 1

---
+++ Lab 1/1.c
@@ -0,0 +1,13 @@
+// area of circle
+#include<stdio.h>
+#define PI 3.14159
+void main()
+{
+    float radius, area;
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+    
+    area = PI * radius * radius;
+    
+    printf("The area of the circle is: %.2f\n", area);
+}
---
+++ Lab 1/7.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    int totalDays, years, months, days;
+    printf("Enter total days: ");
+    scanf("%d", &totalDays);
+    years = totalDays / 365;
+    months = (totalDays % 365) / 30;
+    days = totalDays % 30;
+    printf("Time in YY:MM:DD format: %02d:%02d:%02d\n", years, months, days);
+}
