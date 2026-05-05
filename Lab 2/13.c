453e8b9fefbc331036fc8a8f1f35d5fb8f673f01
Dhruv Limbasiya <24031701027@darshan.ac.in>
2026-05-05 12:21:16 +0530

    Commet add

--- Lab 2/13.c
+++ Lab 2/13.c
@@ -1,7 +1,7 @@
 // WAP to check whether a number is prime or not.
 #include <stdio.h>
 #include <stdbool.h>
-
+// can return 1 or 0 instead of true or false, but using bool makes it more readable and easier to understand
 int isPrime(int number)
 {
     for (int i = 2; i <= (number / 2) + 1; i++)
