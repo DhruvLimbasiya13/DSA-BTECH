f03e46953af8f0de9f4190182a6fccd46311c91d
Dhruv Limbasiya <24031701027@darshan.ac.in>
2026-06-15 10:40:30 +0530

    Lab 1

---
+++ Lab 1/A2_2.c
@@ -0,0 +1,12 @@
+//whether the number is even or odd
+#include <stdio.h>
+void main()
+{
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    if(num % 2 == 0)
+        printf("%d is an even number.\n", num);
+    else
+        printf("%d is an odd number.\n", num);
+}
